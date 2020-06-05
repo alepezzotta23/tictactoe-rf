c1aa51a0e7d53b11c8afa340a5a80f9c75794e25
Alessandro Pezzotta <alessandro.pezzotta23@gmail.com>
2020-06-05 22:42:29 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -1,95 +1,111 @@
-const http = require("http")
-const express = require("express");
-const app = express();
-const socketIo = require("socket.io");
-const fs = require("fs");
+/* eslint-disable prettier/prettier */
+/* eslint-disable consistent-return */
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+/* eslint-disable func-names */
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+/* eslint-disable prettier/prettier */
+const http = require('http')
+const express = require('express')
 
-const server = http.Server(app).listen(8080);
-const io = socketIo(server);
-const clients = {};
+const app = express()
+const socketIo = require('socket.io')
+const fs = require('fs')
+
+const server = http.Server(app).listen(8080)
+const io = socketIo(server)
+const clients = {}
 
 // Serve static resources
-app.use(express.static(__dirname + "/../client/"));
-app.use(express.static(__dirname + "/../node_modules/"));
+app.use(express.static(`${__dirname}/../client/`))
+app.use(express.static(`${__dirname}/../node_modules/`))
 
-app.get("/", (req, res) => {
-    const stream = fs.createReadStream(__dirname + "/../client/index.html");
-    stream.pipe(res);
-});
+app.get('/', (req, res) => {
+    const stream = fs.createReadStream(`${__dirname}/../client/index.html`)
+    stream.pipe(res)
+})
 
-var players = {}; // opponent: scoket.id of the opponent, symbol = "X" | "O", socket: player's socket
-var unmatched;
+app.get('/five', (req, res) => {
+    const stream = fs.createReadStream(`${__dirname}/../client/index5.html`)
+    stream.pipe(res)
+})
 
+const players = {} // opponent: scoket.id of the opponent, symbol = "X" | "O", socket: player's socket
+let unmatched
 
 // When a client connects
-io.on("connection", function(socket) {
-    let id = socket.id;
+io.on('connection', function (socket) {
+    const { id } = socket
 
-    console.log("New client connected. ID: ", socket.id);
-    clients[socket.id] = socket;
+    console.log('New client connected. ID: ', socket.id)
+    clients[socket.id] = socket
 
-    socket.on("disconnect", () => {// Bind event for that socket (player)
-        console.log("Client disconnected. ID: ", socket.id);
-        delete clients[socket.id];
-        socket.broadcast.emit("clientdisconnect", id);
-    });
+    socket.on('disconnect', () => {
+        // Bind event for that socket (player)
+        console.log('Client disconnected. ID: ', socket.id)
+        delete clients[socket.id]
+        socket.broadcast.emit('clientdisconnect', id)
+    })
 
-    join(socket); // Fill 'players' data structure
+    join(socket) // Fill 'players' data structure
 
-    if (opponentOf(socket)) { // If the current player has an opponent the game can begin
-        socket.emit("game.begin", { // Send the game.begin event to the player
-            symbol: players[socket.id].symbol
-        });
+    if (opponentOf(socket)) {
+        // If the current player has an opponent the game can begin
+        socket.emit('game.begin', {
+            // Send the game.begin event to the player
+            symbol: players[socket.id].symbol,
+        })
 
-        opponentOf(socket).emit("game.begin", { // Send the game.begin event to the opponent
-            symbol: players[opponentOf(socket).id].symbol 
-        });
+        opponentOf(socket).emit('game.begin', {
+            // Send the game.begin event to the opponent
+            symbol: players[opponentOf(socket).id].symbol,
+        })
     }
 
-
     // Event for when any player makes a move
-    socket.on("make.move", function(data) {
+    socket.on('make.move', function (data) {
         if (!opponentOf(socket)) {
             // This shouldn't be possible since if a player doens't have an opponent the game board is disabled
-            return;
+            return
         }
 
         // Validation of the moves can be done here
 
-        socket.emit("move.made", data); // Emit for the player who made the move
-        opponentOf(socket).emit("move.made", data); // Emit for the opponent
-    });
+        socket.emit('move.made', data) // Emit for the player who made the move
+        opponentOf(socket).emit('move.made', data) // Emit for the opponent
+    })
 
     // Event to inform player that the opponent left
-    socket.on("disconnect", function() {
+    socket.on('disconnect', function () {
         if (opponentOf(socket)) {
-        opponentOf(socket).emit("opponent.left");
+            opponentOf(socket).emit('opponent.left')
         }
-    });
-});
-
+    })
+})
 
 function join(socket) {
     players[socket.id] = {
         opponent: unmatched,
-        symbol: "X",
-        socket: socket
-    };
+        symbol: 'X',
+        socket,
+    }
 
     // If 'unmatched' is defined it contains the socket.id of the player who was waiting for an opponent
     // then, the current socket is player #2
-    if (unmatched) { 
-        players[socket.id].symbol = "O";
-        players[unmatched].opponent = socket.id;
-        unmatched = null;
-    } else { //If 'unmatched' is not define it means the player (current socket) is waiting for an opponent (player #1)
-        unmatched = socket.id;
+    if (unmatched) {
+        players[socket.id].symbol = 'O'
+        players[unmatched].opponent = socket.id
+        unmatched = null
+    } else {
+        // If 'unmatched' is not define it means the player (current socket) is waiting for an opponent (player #1)
+        unmatched = socket.id
     }
 }
 
 function opponentOf(socket) {
     if (!players[socket.id].opponent) {
-        return;
+        return
     }
-    return players[players[socket.id].opponent].socket;
+    return players[players[socket.id].opponent].socket
 }
